ca84a62ac6ffc6676aac282fa22d00281928987f
UglanovEA <uglanovea@gmail.com>
2021-11-22 17:46:18 +0300

    ver 2.0

--- DarkInk/js/app.js
+++ DarkInk/js/app.js
@@ -94,4 +94,50 @@ new Swiper(".gallery__slider", {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
 	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		460: {
+			slidesPerView: 1.2,
+		},
+		600: {
+			slidesPerView: 1.6,
+		},
+		725: {
+			slidesPerView: 2,
+		},
+		1000: {
+			slidesPerView: 2.5,
+		},
+		1171: {
+			slidesPerView: 3,
+		},
+	},
+});
+
+$(function () {
+	var form = $(".form__wrap");
+	var label = $(".form__label");
+	var input = $(".form__input");
+	var err = $(".form__error");
+
+	form.on("click", "label", function () {
+		$(this).addClass("form__label_active");
+	});
+	input.on("blur", function () {
+		var attr = $(this).attr("data-err");
+		if (!$(this).val()) {
+			$(this).css("border", "1px solid #f13f3f");
+			err.eq(attr).css("display", "block");
+		} else {
+			$(this).css("border", "1px solid #a1a1a1");
+			err.eq(attr).css("display", "none");
+		}
+		for (var i = 0; i < input.length; i++) {
+			if (!input.eq(i).val()) {
+				label.eq(i).removeClass("form__label_active");
+			}
+		}
+	});
 });
