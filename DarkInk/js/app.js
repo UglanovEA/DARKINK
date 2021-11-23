4733933a8c1f62fef4832b3a53d8a8a8e52b7e49
UglanovEA <uglanovea@gmail.com>
2021-11-23 16:19:15 +0300

    ver 2.1

--- DarkInk/js/app.js
+++ DarkInk/js/app.js
@@ -1,4 +1,17 @@
 $(function () {
+//hide preloader
+$(".preloader").fadeOut();
+
+//animation burger line
+var headerBurger = $(".header__burger"),
+	headerMenu = $(".header__nav");
+
+headerBurger.on("click", function () {
+	$(this).toggleClass("open");
+	headerMenu.toggleClass("slide");
+})};
+
+$(function () {
 	//animation burger line
 	var headerBurger = $(".header__burger"),
 		headerMenu = $(".header__nav");
@@ -139,5 +152,40 @@ $(function () {
 				label.eq(i).removeClass("form__label_active");
 			}
 		}
+	});
+});
+
+$(function () {
+	var html = $("html");
+	var btnUp = $(".scrollTop");
+	var whatWeDoImg = $(".whatWeDo__img");
+	var whatWeDoTitle = $(".whatWeDo__title");
+	var callbackImg = $(".callback__img");
+	var callbackTitle = $(".callback__title");
+
+	$(window).on("scroll", function () {
+		//show buttonUp
+		if (html.scrollTop() > 500) {
+			btnUp.fadeIn();
+		} else {
+			btnUp.fadeOut();
+		}
+
+		//show img block
+		if (html.scrollTop() >= whatWeDoTitle.offset().top - 500 && $(window).width() > 1024) {
+			whatWeDoImg.fadeIn(1000);
+		}
+		if (html.scrollTop() >= callbackTitle.offset().top - 500 && $(window).width() > 1024) {
+			callbackImg.fadeIn(1000);
+		}
+	});
+
+	btnUp.on("click", function () {
+		html.animate(
+			{
+				scrollTop: 0,
+			},
+			500
+		);
 	});
 });
